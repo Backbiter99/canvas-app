785b62727d6f65199557dd0b9096cb34def646d6
Prajwal Jha <prajwaljha@gmail.com>
2025-02-12 23:45:53 +0530

    add common, backend-common and db package

--- apps/backend-http/src/routes.ts
+++ apps/backend-http/src/routes.ts
@@ -1,23 +1,49 @@
 import express, { Router } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { authMiddleware } from "./middleware";
+import {
+    CreateRoomSchema,
+    CreateUserSchema,
+    SigninSchema,
+} from "@repo/common/types";
 
 const appRouter: Router = express.Router();
 
 appRouter.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            msg: "Incorrect Inputs",
+        });
+        return;
+    }
     const userId = "123";
     res.json({
         userId: userId,
     });
 });
 appRouter.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.status(411).json({
+            msg: "Incorrect Inputs",
+        });
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({ userId }, JWT_SECRET);
 
     res.json({ token: token });
 });
 appRouter.post("/room", authMiddleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            msg: "Incorrect Inputs",
+        });
+        return;
+    }
     res.json({
         roomId: 123,
     });
